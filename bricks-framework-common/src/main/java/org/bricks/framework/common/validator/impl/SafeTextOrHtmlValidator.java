216e2e88dd93f61e5d3d3e62c173ff4fcfd0f9a7
wankaiming <llwan@ewaytec.cn>
2019-05-30 16:52:40 +0800

    update--优化正则表达式

--- bricks-framework-common/src/main/java/org/bricks/framework/common/validator/impl/SafeTextOrHtmlValidator.java
+++ bricks-framework-common/src/main/java/org/bricks/framework/common/validator/impl/SafeTextOrHtmlValidator.java
@@ -15,11 +15,11 @@ import org.bricks.framework.common.validator.SafeTextOrHtml;
  */
 public class SafeTextOrHtmlValidator implements ConstraintValidator<SafeTextOrHtml, String> {
 
-	private static final String patternEvent = "([\\s\\S]*)on([a-zA-Z]+)([\\s\\S]*)\\=([\\s\\S]*)";
+	private static final String patternEvent = "([\\s\\S]*)(onafterprint|onbeforeprint|onbeforeonload|onblur|onerror|onfocus|onhaschange|onload|onmessage|onoffline|ononline|onpagehide|onpageshow|onpopstate|onredo|onresize|onstorage|onundo|onunload|onblur|onchange|oncontextmenu|onfocus|onformchange|onforminput|oninput|oninvalid|onreset|onselect|onsubmit|onkeydown|onkeypress|onkeyup|onclick|ondblclick|ondrag|ondragend|ondragenter|ondragleave|ondragover|ondragstart|ondrop|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|onmousewheel|onscroll|onabort|oncanplay|oncanplaythrough|ondurationchange|onemptied|onended|onerror|onloadeddata|onloadedmetadata|onloadstart|onpause|onplay|onplaying|onprogress|onratechange|onreadystatechange|onseeked|onseeking|onstalled|onsuspend|ontimeupdate|onvolumechange|onwaiting|onshow|ontoggle)([\\s]*)\\=([\\s\\S]*)";
 
-	private static final String patternScrpit = "([\\s\\S]*)\\<([\\s\\S]*)script([\\s\\S]*)\\>([\\s\\S]*)\\<([\\s\\S]*)\\/([\\s\\S]*)script([\\s\\S]*)\\>([\\s\\S]*)";
+	private static final String patternScrpit = "([\\s\\S]*)\\<([\\s]*)script([\\s]*)\\>([\\s\\S]*)\\<([\\s]*)\\/([\\s]*)script([\\s]*)\\>([\\s\\S]*)";
 
-	private static final String patternJavaScrpit = "([\\s\\S]*)href([\\s\\S]*)\\=([\\s\\S]*)(['|\"]?)([\\s\\S]*)javascript([\\s\\S]*)\\:([\\s\\S]*)";
+	private static final String patternJavaScrpit = "([\\s\\S]*)javascript([\\s]*)\\:([\\s\\S]*)";
 
 	private static final Pattern patternEventObj = Pattern.compile(patternEvent, Pattern.CASE_INSENSITIVE);
 
