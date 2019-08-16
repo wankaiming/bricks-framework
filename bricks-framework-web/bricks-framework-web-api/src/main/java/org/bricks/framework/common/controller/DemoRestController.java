4b023fd4301e836e070767153999ac41394ea63d
wankaiming <llwan@ewaytec.cn>
2019-08-16 14:06:47 +0800

    add--增加redis使用示例

--- bricks-framework-web/bricks-framework-web-api/src/main/java/org/bricks/framework/common/controller/DemoRestController.java
+++ bricks-framework-web/bricks-framework-web-api/src/main/java/org/bricks/framework/common/controller/DemoRestController.java
@@ -7,10 +7,13 @@ import org.bricks.framework.common.dto.BaseDto;
 import org.bricks.framework.common.dto.DemoTestAddParamDto;
 import org.bricks.framework.common.dto.DemoTestDto;
 import org.bricks.framework.common.utils.ValidationErrorMsgGetUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/demo")
 @RefreshScope
 public class DemoRestController extends BaseRestController {
+	
+	private static final Logger log = LoggerFactory.getLogger(DemoRestController.class);
 
 	@Value("${name:哈哈}")
 	private String name;
 	
 	@Autowired
 	private DemoEsRepository demoEsRepository;
+	
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	@ResponseBody
@@ -44,12 +52,17 @@ public class DemoRestController extends BaseRestController {
 		DemoTestDto DemoTestDto = new DemoTestDto();
 		BeanUtils.copyProperties(param, DemoTestDto);
 		demoEsRepository.save(DemoTestDto);
+		
+		redisTemplate.opsForValue().set("name", name);
 		return new BaseDto();
 	}
 
 	@RequestMapping("/query/{id}")
 	public DemoTestDto query(@PathVariable("id") String id) {
 		DemoTestDto demoInfo = demoEsRepository.queryDemoInfoById(id);
+
+		String result = (String) redisTemplate.opsForValue().get("name");
+		log.info("redis result:{}",result);
 		return demoInfo;
 	}
 
